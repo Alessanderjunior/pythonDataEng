e6f0b0624b0406512e95628f7bac6dd5f54c6bfe
alessander.junior <alessander.junior@opah.com.br>
2021-07-29 14:53:37 -0300

    Transform Data v1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import sqlalchemy
-import pandas as pd
+import sqlalchemy  
+import pandas as  pd
 from sqlalchemy.orm import sessionmaker
 import requests
 import json
 from datetime import datetime
 import datetime
-import _sqlite3
+import sqlite3
 
 DATABASE_LOCATION = 'sqlite:///my_played_tracks.sqlite'
 USER_ID = 'junioreriolwua'
@@ -32,4 +32,29 @@ if __name__ == "__main__":
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
 
     data = r.json()
-    print(data)
+   
+   
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
+
+    # Extracting only the relevant bits of data from the json object      
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
+        
+    # Prepare a dictionary in order to turn it into a pandas dataframe below       
+    song_dict = {
+        "song_name" : song_names,
+        "artist_name": artist_names,
+        "played_at" : played_at_list,
+        "timestamp" : timestamps
+    }
+
+    song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+
+     
+    print(song_df)
